559ea2fa0ddbb8c6d0444ebcf19e55f1fa38e76d
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2019-07-03 09:03:22 -0300

    Hardware.ino pronto para teste dos nós garras/motores

--- Arduino/Hardware/Hardware.ino
+++ Arduino/Hardware/Hardware.ino
@@ -7,7 +7,7 @@
 
 ros::NodeHandle nh;
 
-
+/*
 byakugan::RefletanciaMsg dataRefletancia;
 ros::Publisher pubRefletancia("refletancia", &dataRefletancia);
 
@@ -16,7 +16,7 @@ ros::Publisher pubSonares("distancia", &dataSonares);
 
 byakugan::BotoesMsg dataBotoes;
 ros::Publisher pubBotoes("botoes", &dataBotoes);
-/*
+
 std_msgs::Float64MultiArray dataSensoresCor;
 ros::Publisher pubSensoresCor("cor", &dataSensoresCor);
 */
@@ -24,12 +24,13 @@ ros::Publisher pubSensoresCor("cor", &dataSensoresCor);
 byakugan::PosicaoMsg dataPosicao;
 ros::Publisher pubPosicao("posicao", &dataPosicao)
 */
-void motoresCb(std_msgs::Int32MultiArray motores){
+
+void motoresCb(const std_msgs::Int32MultiArray &motores){
   robo.acionarMotores(motores.data[0], motores.data[1]);
 }
 ros::Subscriber<std_msgs::Int32MultiArray> subMotores("motores", &motoresCb);
 
-void garraCb(std_msgs::Int32MultiArray garra){
+void garraCb(const std_msgs::Int32MultiArray &garra){
   robo.acionarServoGarra1(garra.data[0]);
   robo.acionarServoGarra2(garra.data[1]);
 }
@@ -40,11 +41,11 @@ void setup() {
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(subMotores);
-  nh.subscribe(subGarra);
+  //nh.subscribe(subGarra);
 
-  nh.advertise(pubRefletancia);
-  nh.advertise(pubSonares);
-  nh.advertise(pubBotoes);
+  //nh.advertise(pubRefletancia);
+  //nh.advertise(pubSonares);
+  //nh.advertise(pubBotoes);
   //nh.advertise(pubSensoresCor);
 
   robo.configurar(true);
@@ -53,7 +54,7 @@ void setup() {
 }
 
 void loop() {
-  dataRefletancia.refletancia[0] = robo.lerSensorLinhaMaisEsqSemRuido();
+  /*dataRefletancia.refletancia[0] = robo.lerSensorLinhaMaisEsqSemRuido();
   dataRefletancia.refletancia[1] = robo.lerSensorLinhaEsqSemRuido();
   dataRefletancia.refletancia[2] = robo.lerSensorLinhaDirSemRuido();
   dataRefletancia.refletancia[3] = robo.lerSensorLinhaMaisDirSemRuido();
@@ -61,19 +62,19 @@ void loop() {
   dataSonares.sensoresDistancia[0] = robo.lerSensorSonarFrontal();
   dataSonares.sensoresDistancia[1] = robo.lerSensorSonarDir();
   dataSonares.sensoresDistancia[2] = robo.lerSensorSonarEsq();
-  /*
+  *//*
   dataSensoresCor.data[0] = robo.getHSVEsquerdo().h;
   dataSensoresCor.data[1] = robo.getHSVEsquerdo().s;
   dataSensoresCor.data[2] = robo.getHSVEsquerdo().v;
   */
-  dataBotoes.botao1.data = robo.botao1Pressionado();
+  /*dataBotoes.botao1.data = robo.botao1Pressionado();
   dataBotoes.botao2.data = robo.botao2Pressionado();
   dataBotoes.botao3.data = robo.botao3Pressionado();
 
   pubRefletancia.publish(&dataRefletancia);
   pubSonares.publish(&dataSonares);
-  pubBotoes.publish(&dataBotoes);
-//  pubSensoresCor.publish(&dataSensoresCor);
+  pubBotoes.publish(&dataBotoes);*/
+  //pubSensoresCor.publish(&dataSensoresCor);
 
   nh.spinOnce();
 }

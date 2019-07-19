e8949b7a0f4e48f308b52619bec608b48517998e
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2019-07-19 08:32:18 -0300

    LED_BUILTIN como sinalizador

--- Arduino/Hardware/Hardware.ino
+++ Arduino/Hardware/Hardware.ino
@@ -1,4 +1,3 @@
-
 #include <robo_hardware2.h>
 #include <ros.h>
 #include <std_msgs/Int32MultiArray.h>
@@ -7,15 +6,21 @@ ros::NodeHandle nh;
 
 void motoresCb(const std_msgs::Int32MultiArray &motores){
   robo.acionarMotores(motores.data[0], motores.data[1]);
+  if (motores.data[0] > 0) {
+    digitalWrite(LED_BUILTIN, 1);
+  } else {
+    digitalWrite(LED_BUILTIN, 0);
+  }
 }
 ros::Subscriber<std_msgs::Int32MultiArray> subMotores("ctrl_motores", &motoresCb);
 
+/*
 void garraCb(const std_msgs::Int32MultiArray &garra){
   robo.acionarServoGarra1(garra.data[0]);
   robo.acionarServoGarra2(garra.data[1]);
 }
 ros::Subscriber<std_msgs::Int32MultiArray> subGarras("ctrl_garras", &garraCb);
-
+*/
 
 void setup() {
   nh.getHardware()->setBaud(115200);

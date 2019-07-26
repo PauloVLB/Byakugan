97c43b202ba008e705564fc2703d36d8a1ceb2ae
PauloVLB <vitorlimaborges@gmail.com>
2019-07-26 10:28:44 -0300

    Improvements had been made

--- byakugan/scripts/Classes/Sensores.py
+++ byakugan/scripts/Classes/Sensores.py
@@ -2,73 +2,74 @@
 
 import rospy
 import message_filters
+import threading
 from byakugan.msg import SensoresDistanciaMsg, RefletanciaMsg, BoolStamped, BotoesMsg
 
 class Sensores:
+    '''
     __refletancia
     __distancia
     __botoes
-    lock
-    lockRefle = []
-    lockDist = []
-    lockBtns = []
-
-    def __init__(self):
+    __lock
+    __lockRefle = []
+    __lockDist = []
+    __lockBtns = []
+    '''
+    def __init__(self, nRefle=4, nDist=3, nBtns=3):
         self.__refletancia = RefletanciaMsg()
         self.__distancia = SensoresDistanMsg()
         self.__botoes = BotoesMsg()
 
+        self.__lock = threading.Lock()
+        self.__lockRefle = [threading.Lock()] * nRefle
+        self.__lockDist = [threading.Lock()] * nDist
+        self.__lockBtns = [threading.Lock()] * nBtns
+
     def setValues(self, refle, dist, btns):
-        global lock
-        lock.aquire()
+        self.__lock.aquire()
 
         self.__refletancia = refle
         self.__distancia = dist
         self.__botoes = btns
 
-        lock.release()
+        self.__lock.release()
 
     def getRefle(self, n):
-        global lockRefle
-        lockRefle[n].acquire() #pega o lock de determinada variável de refletancia
+        self.__lockRefle[n].acquire() #pega o lock de determinada variável de refletancia
 
         refleValue = self.__refletancia.refletancia[n] #lê a variável
 
-        lockRefle[n].release() #solta o lock
+        self.__lockRefle[n].release() #solta o lock
         return refleValue
 
     def getDist(self, n):
-        global lockDist
-        lockDist[n].acquire() #pega o lock de determinada variável de distancia
+        self.__lockDist[n].acquire() #pega o lock de determinada variável de distancia
 
         distValue = self.__distancia.sensoresDistancia[n] #lê a variável
 
-        lockDist[n].release() #solta o lock
+        self.__lockDist[n].release() #solta o lock
         return distValue
 
     def getBtn1(self):
-        global lockBtns
-        lockBtns[0].acquire()
+        self.__lockBtns[0].acquire()
 
         btn1Value = self.__botoes.botao1.data
 
-        lockBtns[0].release()
+        self.__lockBtns[0].release()
         return btn1Value
 
     def getBtn2(self):
-        global lockBtns
-        lockBtns[1].acquire()
+        self.__lockBtns[1].acquire()
 
         btn2Value = self.__botoes.botao2.data
 
-        lockBtns[1].release()
+        self.__lockBtns[1].release()
         return btn2Value
 
     def getBtn3(self):
-        global lockBtns
-        lockBtns[2].acquire()
+        self.__lockBtns[2].acquire()
 
         btn3Value = self.__botoes.botao3.data
 
-        lockBtns[2].release()
+        self.__lockBtns[2].release()
         return btn3Value

8b3387653846bb5bd23db4d5871c89ca75cd0bb4
PauloVLB <vitorlimaborges@gmail.com>
2019-07-26 22:56:57 -0300

    Locks desnecessários foram removidos.

--- byakugan/scripts/Classes/Sensores.py
+++ byakugan/scripts/Classes/Sensores.py
@@ -11,65 +11,59 @@ class Sensores:
     __distancia
     __botoes
     __lock
-    __lockRefle = []
-    __lockDist = []
-    __lockBtns = []
     '''
-    def __init__(self, nRefle=4, nDist=3, nBtns=3):
-        self.__refletancia = RefletanciaMsg()
-        self.__distancia = SensoresDistanMsg()
-        self.__botoes = BotoesMsg()
-
+    def __init__(self):
         self.__lock = threading.Lock()
-        self.__lockRefle = [threading.Lock()] * nRefle
-        self.__lockDist = [threading.Lock()] * nDist
-        self.__lockBtns = [threading.Lock()] * nBtns
+
+        self.__lock.acquire()
+        try:
+            self.__refletancia = RefletanciaMsg()
+            self.__distancia = SensoresDistanMsg()
+            self.__botoes = BotoesMsg() 
+        finally:
+            self.__lock.release()
 
     def setValues(self, refle, dist, btns):
-        self.__lock.aquire()
-
-        self.__refletancia = refle
-        self.__distancia = dist
-        self.__botoes = btns
-
-        self.__lock.release()
+        self.__lock.acquire()
+        try:
+            self.__refletancia = refle
+            self.__distancia = dist
+            self.__botoes = btns
+        finally:
+            self.__lock.release()
 
     def getRefle(self, n):
-        self.__lockRefle[n].acquire() #pega o lock de determinada variável de refletancia
-
-        refleValue = self.__refletancia.refletancia[n] #lê a variável
-
-        self.__lockRefle[n].release() #solta o lock
-        return refleValue
+        self.__lock.acquire() #pega o lock
+        try:
+            return self.__refletancia.refletancia[n] #lê a variável
+        finally:
+            self.__lock.release() #solta o lock
 
     def getDist(self, n):
-        self.__lockDist[n].acquire() #pega o lock de determinada variável de distancia
+        self.__lock.acquire() #pega o lock
+        try:
+            return self.__distancia.sensoresDistancia[n] #lê a variável
+        finally:
+            self.__lock.release() #solta o lock
 
-        distValue = self.__distancia.sensoresDistancia[n] #lê a variável
-
-        self.__lockDist[n].release() #solta o lock
-        return distValue
 
     def getBtn1(self):
-        self.__lockBtns[0].acquire()
-
-        btn1Value = self.__botoes.botao1.data
-
-        self.__lockBtns[0].release()
-        return btn1Value
+        self.__lock.acquire()
+        try:
+            return self.__botoes.botao1.data
+        finally:
+            self.__lock.release()
 
     def getBtn2(self):
-        self.__lockBtns[1].acquire()
-
-        btn2Value = self.__botoes.botao2.data
-
-        self.__lockBtns[1].release()
-        return btn2Value
+        self.__lock.acquire()
+        try:
+            return self.__botoes.botao2.data
+        finally:
+            self.__lock.release()
 
     def getBtn3(self):
-        self.__lockBtns[2].acquire()
-
-        btn3Value = self.__botoes.botao3.data
-
-        self.__lockBtns[2].release()
-        return btn3Value
+        self.__lock.acquire()
+        try:
+            return self.__botoes.botao3.data
+        finally:
+            self.__lock.release()

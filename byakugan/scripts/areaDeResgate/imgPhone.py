4b4fcea780d12c4e514941b0bca0f93a56ff701b
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2019-08-13 08:48:10 -0300

    Adicionado imgPhone.py para teste com câmera de celular

---
+++ byakugan/scripts/areaDeResgate/imgPhone.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+import requests
+import numpy as np
+
+url = "http://10.0.0.6:8080/shot.jpg"
+
+def talker():
+
+    pub = rospy.Publisher('topico_img', Image, queue_size=10)
+    rospy.init_node('talkerPhone', anonymous=False)
+    rate = rospy.Rate(30) # 30hz
+    bridge = CvBridge()
+    cv2.namedWindow('imgPhone', cv2.WINDOW_NORMAL)
+
+    while not rospy.is_shutdown():
+
+        img_resp = requests.get(url)
+        img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+        frame = cv2.imdecode(img_arr, 1)
+
+        frame = cv2.resize(frame, (400, 400))
+
+        frame = cv2.flip(frame, 180)
+
+        cv2.imshow('imgPhone', frame)
+
+        pub.publish(bridge.cv2_to_imgmsg(frame))
+        rate.sleep()
+
+        cv2.waitKey(1)
+
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass

96cedded0be0f60b9c329dc82fb25b1f7956fbb9
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2019-08-17 09:23:02 -0300

    Fixed identificação da área em visões parciais

--- byakugan/scripts/areaDeResgate/FindRectangle.py
+++ byakugan/scripts/areaDeResgate/FindRectangle.py
@@ -11,48 +11,66 @@ class FindRectangle:
     def __init__(self):
         #self.pub = rospy.Publisher('centroid_rectangle', CentroidObject, queue_size=10, latch=True)
 
-        rospy.init_node('find_rectangle', anonymous=False)
+        #rospy.init_node('find_rectangle', anonymous=False)
+
+        self.img = None
         self.thresh = None
 
         #self.CENTER_WINDOW = (240/2), (320/2)
 
     def callback(self, imgCompressed):
         np_arr = np.fromstring(imgCompressed.data, np.uint8)
-    	imgCV = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    	imgCV = cv2.flip(imgCV, 2)
-        self.find(imgCV)
+    	self.img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    	self.img = cv2.flip(self.img, 2)
+        self.find()
+        self.showImg()
 
-        #cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
-        #cv2.imshow('thresh', self.thresh)
-        cv2.namedWindow('result', cv2.WINDOW_NORMAL)
-        cv2.imshow('result', imgCV)
-        cv2.waitKey(1)
+    def cookImg(self):
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.medianBlur(gray, 5) # interfere?
+        gray = 255 - gray
 
-    def find(self, imgCV):
+        _, self.thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+        _, contours, _= cv2.findContours(self.thresh, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+        return contours
+
+    def find(self):
         try:
-            gray = cv2.cvtColor(imgCV, cv2.COLOR_BGR2GRAY)
-            gray = cv2.medianBlur(gray, 5) # interfere?
-            gray = 255 - gray
+            contours = self.cookImg()
+            self.drawRectangle(contours[len(contours) - 1], 10) # draw contorno da própria img
+            contours = self.cookImg() # cozinha a img novamente separando possíveis contours colados na img inicial
 
-            _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
-            _, contours, _= cv2.findContours(thresh, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+            for i in range(0, len(contours) - 1): # -1 impede de draw contour da propria img
 
-            for cnt in contours: # iteração com cada contorno encontrado
-                cX, cY = self.getCentroid(cnt, imgCV) # pega centro do contorno
+                cnt = contours[i]
 
+                approx = cv2.approxPolyDP(cnt, .03 * cv2.arcLength(cnt, True), True)
+
+                cX, cY = self.getCentroid(cnt, self.img) # pega centro do contorno
                 #print cX, cY
+
                 area = self.getArea(cnt)
+                #print area
 
                 # aproxima pontos do contorno - corners
                 approx = cv2.approxPolyDP(cnt, .03 * cv2.arcLength(cnt, True), True)
+                print approx.ravel()
 
-                if not area > 50000.0 and area > 1000: # gambiarra
-                    if self.isBigDist(approx): # verifica se o contorno é retangulo
-                        # draw centro do contorno e retangulo no contorno
-                        self.drawCentroid(cX, cY, imgCV)
-                        self.drawRectangle(cnt, imgCV)
+                if self.isBigDist(approx): # verifica se o contorno é retangulo
+                    # draw centro do contorno e retangulo no contorno
+                    self.drawCentroid(cX, cY)
+                    self.drawRectangle(cnt, 6)
+
         except: # ???
             print 'error in img'
+
+    def showImg(self):
+        cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
+        cv2.imshow('thresh', self.thresh)
+        cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+        cv2.imshow('result', self.img)
+        cv2.waitKey(0)
 
     def isBigDist(self, approx):
         approxRavel = approx.ravel() # transforma em um vetor
@@ -76,16 +94,18 @@ class FindRectangle:
 
         diferenca = abs(compX - compY)
 
-        return diferenca > 50 # if true é retangulo
+        #print diferenca
 
-    def drawCentroid(self, cX, cY, imgCV):
-        cv2.circle(imgCV, (cX, cY), 5, (0, 255, 0), -1)
+        return diferenca > 20 # if true é retangulo else bola preta
 
-    def drawRectangle(self, cnt, imgCV):
+    def drawCentroid(self, cX, cY):
+        cv2.circle(self.img, (cX, cY), 10, (0, 255, 0), -1)
+
+    def drawRectangle(self, cnt, width):
         rect = cv2.minAreaRect(cnt)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        cv2.drawContours(imgCV,[box],0,(0,255, 0),2)
+        cv2.drawContours(self.img,[box],0,(0,255, 0), width)
 
     def getArea(self, cnt): return cv2.contourArea(cnt)
 
@@ -102,4 +122,7 @@ class FindRectangle:
 
 if __name__ == "__main__":
     fr = FindRectangle()
-    fr.listenerImg()
+    fr.img = cv2.imread('c3v2i1.png', 1)
+    #img = cv2.imread('c1v2i1.png', 1)
+    fr.find()
+    fr.showImg()

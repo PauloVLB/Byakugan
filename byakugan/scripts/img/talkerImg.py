9e0e5abbba7a13fb44df06b9fab91a7c471cba18
PauloVLB <vitorlimaborges@gmail.com>
2019-08-28 08:05:09 -0300

    Nó de disponibilização da imagem da camera atualizado

--- byakugan/scripts/img/talkerImg.py
+++ byakugan/scripts/img/talkerImg.py
@@ -9,28 +9,18 @@ class TalkerImg:
 		rospy.init_node("talkerCamImg", anonymous=False)
 		rospy.loginfo("Starting node " + rospy.get_name())
 
-		self.camera = cv2.VideoCapture(0)
+		self.camera = cv2.VideoCapture(1)
 		
-		
-		self.pubImg = rospy.Publisher("imgCam", Image, queue_size=10)
-		self.bridge = CvBridge()
-		
-		self.rate = rospy.Rate(20)
-		
-		self.pub()
-
-	def pub(self):
-		rospy.loginfo("Publishing cam image")
 		while not rospy.is_shutdown():
 			_, frame = self.camera.read()
 			frame = cv2.resize(frame, (320,240))
 
-			imgMsg = self.bridge.cv2_to_imgmsg(frame, 'bgr8')
-			self.pubImg.publish(imgMsg)
-			
-			self.rate.sleep()
+			cv2.namedWindow('IMG', cv2.WINDOW_NORMAL)
+			cv2.imshow('IMG', frame)
+
+			if cv2.waitKey(1) == ord('s'):
+				break
 		
 
 if __name__ == "__main__":
 	ros_node = TalkerImg()
-	rospy.spin()

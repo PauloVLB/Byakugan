f5377355b683e978e7cf74cbe560c8965609b206
PauloVLB <vitorlimaborges@gmail.com>
2019-08-29 11:32:37 -0300

    Inicio da estrategia para obr

--- byakugan/scripts/Classes/findBalls.py
+++ byakugan/scripts/Classes/findBalls.py
@@ -14,15 +14,19 @@ class FindBalls:
 
         self.achouVitima = False
         self.executou = False
+        self.entrouNaSala = False
+        self.podeExecutar = False
 
         self.pubMotores = rospy.Publisher("cmdMotores", CtrlMotores, queue_size=10)
-        self.pubPegar = rospy.Publisher("initPegar", BoolStamped, queue_size=10, latch=True)
+        self.pubPegar = rospy.Publisher("initPegar", BoolStamped, queue_size=10, latch=True)    
 
         self.cmd = cmdMotores.CmdMotores(self.pubMotores)
         subCoordinates = message_filters.Subscriber('coordenadas_circulos', Vector3Stamped)
         subBall = message_filters.Subscriber('tem_circulos', BoolStamped)
+        subDist = message_filters.Subscriber('distancia', SensoresDistanciaMsg)
+        subBtns = message_filters.Subscriber('botoes', BotoesMsg)
 
-        ts = message_filters.TimeSynchronizer([subCoordinates, subBall], 20)
+        ts = message_filters.TimeSynchronizer([subCoordinates, subBall, subDist, subBtns], 20)
         ts.registerCallback(self.ballsCb)
 
     def initPegar(self):
@@ -31,29 +35,39 @@ class FindBalls:
         self.pubPegar.publish(initData)
         self.executou = True
 
-    def ballsCb(self, coordinates, circle):
+    def ballsCb(self, coordinates, circle, dist, btns):
+        if btns.botao2.data:
+            self.podeExecutar = True
+    
+        if self.podeExecutar:
+            if self.executou == False:
+                x, y, r = coordinates.vector.x, coordinates.vector.y, coordinates.vector.z
 
-        if self.executou == False:
-            x, y, r = coordinates.vector.x, coordinates.vector.y, coordinates.vector.z
+                if(dist.sensoresDistancia[2] > 10):
+                    self.entrouNaSala = True
 
-            if circle.existe.data:
-                self.achouVitima = True
-                if self.achouVitima:
-                    self.cmd.roboDir(.07)
-                    self.cmd.roboAcionarMotores(0, 0)
+                if(self.entrouNaSala):
+                    if circle.existe.data:
+                        self.achouVitima = True
+                        if self.achouVitima:
+                            self.cmd.roboDir(.07)
+                            self.cmd.roboAcionarMotores(0, 0)
 
-                    self.initPegar()
-
-                    '''
-                    if x in numpy.arange(200, 280, 1):
-                        self.cmd.roboAcionarMotores(0, 0)
-                        self.pegarVitima()
-                    '''
+                            #self.initPegar()
+                            rospy.loginfo("achei a tete")
+                            '''
+                            if x in numpy.arange(200, 280, 1):
+                                self.cmd.roboAcionarMotores(0, 0)
+                                self.pegarVitima()
+                            '''
+                    else:
+                        rospy.loginfo("cade a tete")
+                        self.cmd.roboAcionarMotores(-30, 30, .3)
+                else: 
+                    rospy.loginfo("rampa")
+                    self.cmd.roboAcionarMotores(50, 54)
             else:
-                self.cmd.roboAcionarMotores(25, -25)
-        else:
-            self.initPegar()
-            rospy.loginfo('ja executei')
+                self.initPegar()
 
 if __name__ == "__main__":
     node = FindBalls()
